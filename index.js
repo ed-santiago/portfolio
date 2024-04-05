512551f24e6bde6aa39eeb3672743430bc4dd85f
Edmond <edmond.santiago7@gmail.com>
2024-04-05 21:02:20 +1100

    add interactivity and background overlay to when hamburger icon is clicked

---
+++ index.js
@@ -0,0 +1,14 @@
+const hamburgerIcon = document.querySelector("nav button");
+const nav = document.querySelector("nav ul");
+const overlay = document.querySelector("#overlay");
+hamburgerIcon.addEventListener("click", () => {
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+    overlay.classList.remove("overlay");
+    hamburgerIcon.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  } else {
+    nav.style.display = "block";
+    overlay.classList.add("overlay");
+    hamburgerIcon.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  }
+})
